d0689d52e62916eeaeed9e776d8d2c104074b851
D-Stack1628 <84602147+D-Stack1628@users.noreply.github.com>
2022-09-13 14:11:01 +0100

    New Python Notebook Added

---
+++ 1st code Add two Number.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+ 
+sum = a + b
+ 
+print("sum:", sum)
+
+
+# In[ ]:
+
+
+
+
